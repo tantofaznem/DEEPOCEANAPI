5e9258add6eb843ae94abcb01a7c26f264343388
Abd Domingos <abdizzoh@gmail.com>
2022-03-14 15:49:03 +0200

    Create config.php

---
+++ data/config.php
@@ -0,0 +1,24 @@
+[globals]
+DEBUG = 1
+UI = "app/views/"
+TEMP = "data/temp/"
+CACHE = "folder=data/cache/"
+UPLOADS = "data/uploads/"
+AUTOLOADS = "app/;app/lib/;plugins/;3rdparty/"
+LOGS = "app/;app/lib/;plugins/;3rdparty/"
+WWW = ""
+WWW_DOC = ""
+WWW_NEWS = ""
+MYSQL_HOST = ""
+MYSQL_PORT = ""
+MYSQL_DB = ""
+MYSQL_USER = ""
+MYSQL_PASS = ""
+ADMIN_USER = ""
+ADMIN_PASS = ""
+SHOW_LAST_POST = 1
+SHOW_LAST_COMMENTS = 0
+SHOW_PUBLIC_ZONE = 1
+STORE_LOGS = 1
+READONLY = 0
+DEPLOY = 0
